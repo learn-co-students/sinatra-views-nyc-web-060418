bfb370b5c0defb2bf330450f5f3bf04d56a5cafe
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-06-25 16:32:51 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,31 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	# get '/' do
+	# 	"<h1>Hello World</h>"
+	# end
+
 	get '/' do
-		
-	end
+    erb :index
+  end
+
+	# get "/info" do
+  #     "Testing the info page"
+  #   end
+
+	get '/' do
+     erb :index
+   end
+
+   get "/info" do
+     erb :info
+   end
+
+	 get '/' do
+      erb :index
+    end
+
+    get "/info" do
+      erb :dogs
+    end
 end
